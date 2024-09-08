8c7a7cd482da4d913a905f416c759893cff280df
LukhanyoNtsika <138343154+LukhanyoNtsika@users.noreply.github.com>
2024-09-08 22:54:09 +0200

    Add files via upload
    In this file, I have updated the documentation on the functionality of the code but also to better understand its use.

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,13 @@
-age = input("How old are you? :")
+# This file is used to print the age of a user after they have entered their 
+# age in the terminal.
+'''
+To ensure that the user enters their age in the terminal we use the 
+input() function.
+The input() function takes a string as an argument, which is the prompt that 
+the user will see.
+The input() function then returns the string that the user entered.
+'''
+age = input("Welcome to the age printer!How old are you? :")
 
 print(age)
 
